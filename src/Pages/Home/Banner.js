3a2fb3df73e54c32569c7c2f979d625e7baebb9b
wpshamim <wpshamim@gmail.com>
2022-07-19 15:17:52 +0600

    Banner Added on top of the page

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is Home page Banner</h1>
-        </div>
+        <section className="banner h-screen">
+            <div class="hero min-h-screen">
+                <div class="hero-content flex-col lg:flex-row">
+                    <div className="text-white p-10">
+                        <h5 className="font-bold mb-5 text-xl">PRO-TRUSTED PERFORMANCE</h5>
+                        <h1 className="text-7xl font-bold leading-snug">
+                            BATTERY-POWERED <br /> OUTDOOR EQUIPMENT
+                        </h1>
+                        <button className="btn btn-primary mt-10"> Read more</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,23 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
     },
     daisyui: {
         themes: [
             {
                 light: {
                     primary: "#FEBD17",
-                    secondary: "#000000",
-                    accent: "#37cdbe",
+                    secondary: "#1B1B1B",
+                    accent: "#9c9c9c",
                     neutral: "#3d4451",
                     "base-100": "#ffffff",
                     "--rounded-btn": "none",
