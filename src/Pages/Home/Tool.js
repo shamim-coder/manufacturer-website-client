3c53160f2191ac9b95aeafc33356769652db87b9
wpshamim <wpshamim@gmail.com>
2022-07-21 23:01:09 +0600

    showing tools on home page as latest tools

---
+++ src/Pages/Home/Tool.js
@@ -0,0 +1,36 @@
+import { format } from "date-fns/esm";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Tool = ({ tool }) => {
+    const navigate = useNavigate();
+
+    const { price, name, model, image, availableStock, issueDate, _id } = tool;
+
+    const today = format(new Date(), "MM-dd-yyyy");
+
+    return (
+        <div className="mb-5">
+            <div className="relative">
+                <span className={`uppercase ${availableStock === 0 ? "bg-gray-300 text-gray-700" : today === issueDate ? "bg-primary text-secondary" : "bg-secondary text-white"} px-2 py-1 absolute top-[-15px] left-[15px] font-medium z-50`}>
+                    {availableStock === 0 ? "SOLD" : today === issueDate ? "NEW" : "SALE"}
+                </span>
+                <div className=" bg-white relative tool transition overflow-hidden">
+                    <img className="border-2 border-gray-400 p-5" src={image} alt="" />
+                    <button onClick={() => navigate(`/tool-details/${_id}`)} className="transition buy-now btn btn-primary w-full">
+                        {availableStock === 0 ? "See Details" : "Buy Now"}
+                    </button>
+                </div>
+            </div>
+            <h5 className="text-base text-accent mt-2">{model}</h5>
+            <div className="flex gap-10 justify-between mt-3">
+                <Link to={`/tool-details/${_id}`}>
+                    <h3 className="text-xl font-semibold transition hover:text-primary">{name}</h3>
+                </Link>
+                <h3 className="text-2xl font-semibold">${price}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Tool;
