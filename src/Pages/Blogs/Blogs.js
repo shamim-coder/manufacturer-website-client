1abc8782264cd3a7c2a002906e73d505994137f1
wpshamim <wpshamim@gmail.com>
2022-07-22 23:49:30 +0600

    update

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,14 +1,72 @@
 import React from "react";
 import SectionTop from "../../Components/SectionTop/SectionTop";
+import "./Blogs.css";
 
 const Blogs = () => {
     return (
-        <section>
-            <div className="container max-auto">
-                <SectionTop title={"Blogs"} />
+        <section className="blogs py-10">
+            <SectionTop title={"Blogs"} />
+            <div className="container max-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
                 <article>
-                    <h1>How will you improve the performance of a React Application</h1>
-                    <p></p>
+                    <h2>What are the different ways to manage a state is a React application?</h2>
+                    <p>3 Ways To Handle State Better in React</p>
+                    <h4>Custom Hooks</h4>
+                    <p>When using React hooks, you can sometimes end up with incredibly complex stateful logic within a single component that uses multiple types of hooks to achieve a single purpose.</p>
+
+                    <h4>Global State Management</h4>
+                    <p>In most cases, you don't need a government library. Only in larger applications that deal with complex states should you implement an external library to manage them.</p>
+
+                    <h4>Use useReducer for Complex State</h4>
+                    <p>
+                        Sometimes the <code>useState</code> hook won't cut it, especially when you're dealing with complex stateful behavior that can involve large objects. The <code>useReducer</code> hook is a powerful React hook for solving complex
+                        state management that doesn't require third-party dependencies.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>What is a unit test? Why should write unit tests?</h2>
+                    <h4>What is Unit Testing?</h4>
+                    <p>
+                        Unit testing is a type of software testing in which individual units or software components are tested. Its purpose is to verify that each unit of code works as expected. A unit can be anything you want - a line of code, a
+                        method, or a class.
+                    </p>
+                    <p>In general, smaller tests are better because they provide a more detailed view of your code's performance. Also, when you test very small units, your tests can run fast, like a thousand tests per second fast.</p>
+
+                    <h4>Why Do We Need Unit Testing?</h4>
+
+                    <p>
+                        Unit tests save time and money. We usually tend to test the happy path more than the unhappy path. If you release such an application without thorough testing, you will constantly have to fix the problems that your potential
+                        users have pointed out. The time to resolve these issues could have been used to create new features or optimize the existing system. Keep in mind that fixing bugs without running tests can also introduce new bugs into the
+                        system.
+                    </p>
+                </article>
+
+                <article>
+                    <h2>What are the different ways to manage a state is a React application?</h2>
+                    <p>In JavaScript, objects inherit properties from other objects — prototypes. This is the idea of prototypic inheritance.</p>
+                    <p>JavaScript looks for inherited properties in the prototype of the object, but also in the prototype of the prototype, and so on in the prototype chain.</p>
+                    <p>While prototype inheritance seems clunky at first, once you understand it you can enjoy its simplicity and possibilities. Objects inherit properties from objects - what could be simpler?</p>
+                </article>
+
+                <article>
+                    <h2>
+                        Why you do not set the state directly in React. For example, if you have const <code>[products, setProducts] = useState([])</code>. Why you do not set <code>products = [...]</code> instead, you use the <code>setProducts</code>
+                    </h2>
+                    <p>
+                        Because simply re-assigning a variable has no way of triggering any sort of action in the framework, so it wouldn't trigger a re-render. You certainly could re-assign a (non-const) variable in any given render. But all it
+                        would do is change the value of that variable for that single execution of that function.
+                    </p>
+                </article>
+                <article>
+                    <h2>How will you improve the performance of a React Application?</h2>
+                    <p>
+                        Using binding functions in constructors: By adding an arrow function to a class, we add it as an object and not as a prototype property of the class. And if we use the component multiple times, there will be different
+                        instances of these functions in each component object. The most reliable way to use functions is to bind them to a constructor.
+                    </p>
+                    <p>
+                        Avoid inline style attributes: The browser often invests a lot of time in rendering when styles are implied to be inline. Scripting and rendering takes time because the browser has to schedule all React style rules according
+                        to CSS properties. Creating a separate style.js file and importing it into the component is a faster method.
+                    </p>
                 </article>
             </div>
         </section>
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,10 +1,162 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faLinkedin, faGithub, faTwitter, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
+
 const Portfolio = () => {
     return (
-        <div>
-            <h1>This is Portfolio</h1>
-        </div>
+        <section className="container mx-auto">
+            <div className="sm:flex gap-10 my-10">
+                <div className="relative profile-image mb-5">
+                    <img className="sm:w-[200px] sm:h-[200px] object-cover shadow-lg p-2" src="https://i.ibb.co/gwNLD60/1649771387569.jpg" alt="" />
+                </div>
+                <div>
+                    <h2 className="text-2xl font-semibold mb-1">Shamim Reza</h2>
+                    <p className="capitalize">Front-end Web Developer</p>
+
+                    <div className="mt-10">
+                        <h4 className="text-xl mb-3">Connect with me </h4>
+                        <div className="flex gap-4 text-xl">
+                            <a className="tooltip tooltip-primary" data-tip="Facebook" href={"https://www.facebook.com/shamimreza.dev/"}>
+                                <FontAwesomeIcon icon={faFacebookF} />
+                            </a>
+
+                            <a className="tooltip tooltip-primary" data-tip="LinkedIn" href={"https://www.linkedin.com/in/wpshamim/"}>
+                                <FontAwesomeIcon icon={faLinkedin} />
+                            </a>
+
+                            <a className="tooltip tooltip-primary" data-tip="Github" href={"https://github.com/wpshamim"}>
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+
+                            <a className="tooltip tooltip-primary" data-tip="Twitter" href={"https://twitter.com/wpshamim"}>
+                                <FontAwesomeIcon icon={faTwitter} />
+                            </a>
+                            <a className="tooltip tooltip-primary" data-tip="Stack Overflow" href={"https://stackoverflow.com/users/14978156/shamim-reza"}>
+                                <FontAwesomeIcon icon={faStackOverflow} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-20 my-10">
+                <div className="order-2 sm:order-1">
+                    <div className="education mb-5">
+                        <h3 className="mb-3 text-2xl font-semibold">Education</h3>
+                        <div className="mb-5">
+                            <h4>Secondary School Certificate</h4>
+                            <p>2006</p>
+                            <p>Humanities</p>
+                        </div>
+
+                        <div>
+                            <h4>Higher Secondary Certificate</h4>
+                            <p>2014</p>
+                            <p>Statistic</p>
+                        </div>
+                    </div>
+                    <div className="skill">
+                        <h3 className="mb-3 text-2xl font-semibold">Skill</h3>
+
+                        <div>
+                            <h4>HTML5</h4>
+                            <h4>JavaScript</h4>
+                            <h4>React.js</h4>
+                            <h4>Tailwind CSS</h4>
+                            <h4>Node.js</h4>
+                            <h4>Express js</h4>
+                            <h4>mongoDB</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="order-1 sm:order-2">
+                    <h3 className="mb-3 text-2xl font-semibold">Contact Information </h3>
+                    <table className="mb-5">
+                        <tbody>
+                            <tr className="leading-loose">
+                                <td className="w-28">Phone: </td>
+                                <td>
+                                    <a className="font-body text-blue-500" href={`tel:+:01718860139`}>
+                                        +880 1718 860139
+                                    </a>
+                                </td>
+                            </tr>
+
+                            <tr className="leading-loose">
+                                <td className="w-28">Address: </td>
+                                <td>{"CB-10, Old Kachukhet Bazar, Dhaka Cantonment, Dhaka-1206"}</td>
+                            </tr>
+
+                            <tr className="leading-loose">
+                                <td className="w-28">Email: </td>
+                                <td>
+                                    <a className="font-body text-blue-500" href={`mailto:wpshamim@gmail.com`}>
+                                        {"wpshamim@gmail.com"}
+                                    </a>
+                                </td>
+                            </tr>
+
+                            <tr className="leading-loose">
+                                <td className="w-28">Website: </td>
+                                <td>
+                                    <a className="font-body text-blue-500" href={`https://shamimreza.me`}>
+                                        {"www.shamimreza.me"}
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <h3 className="mb-3 text-2xl font-semibold">Basic Information </h3>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className="w-28">Birthday: </td>
+                                <td>8th September 1991</td>
+                            </tr>
+                            <tr>
+                                <td>Gender: </td>
+                                <td>Male</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="my-14">
+                <h3 className="mb-3 text-2xl font-semibold">Latest Project</h3>
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                    <div className="div">
+                        <img src="https://i.ibb.co/gPFtjBc/Screenshot-2.png" alt="" />
+                        <h2 className="text-2xl">Ema John Ecommerce</h2>
+                        <p>
+                            Live Url: <a href="https://ema-john-shamim.web.app/">https://ema-john-shamim.web.app</a>
+                        </p>
+                        <p>
+                            Github Url: <a href="https://github.com/wpshamim">https://github.com/wpshamim</a>
+                        </p>
+                    </div>
+                    <div className="div">
+                        <img src="https://i.ibb.co/rFcRkXP/Screenshot-7.png" alt="" />
+                        <h2 className="text-2xl">IGC Immigration</h2>
+                        <p>
+                            Live Url: <a href="https://igc-immigration.web.app/">https://igc-immigration.web.app</a>
+                        </p>
+                        <p>
+                            Github Url: <a href="https://github.com/wpshamim">https://github.com/wpshamim</a>
+                        </p>
+                    </div>
+                    <div className="div">
+                        <img src="https://i.ibb.co/bHTT7S4/Screenshot-13.png" alt="" />
+                        <h2 className="text-2xl">Hotel Booking Service</h2>
+                        <p>
+                            Live Url: <a href="https://hotel-booking-sk.web.app/">https://igc-immigration.web.app</a>
+                        </p>
+                        <p>
+                            Github Url: <a href="https://github.com/wpshamim/hotel-specials-client">https://github.com/wpshamim</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
