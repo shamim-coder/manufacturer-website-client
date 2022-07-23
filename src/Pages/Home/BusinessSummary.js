ed0afe1b6c4c8661f97400f94d3ceee3705a916d
wpshamim <wpshamim@gmail.com>
2022-07-23 23:05:34 +0600

    implement countup animation and react visibility sensor

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -1,5 +1,7 @@
 import React from "react";
 import SectionTop from "../../Components/SectionTop/SectionTop";
+import CountUp from "react-countup";
+import Counter from "../../Components/Counter/Counter";
 
 const BusinessSummary = () => {
     return (
@@ -10,23 +12,32 @@ const BusinessSummary = () => {
                 <div className="grid grid-cols-2 md:grid-cols-4 my-10">
                     <div className="item text-center leading-loose mb-10">
                         <i className="fa-solid fa-flag text-6xl text-primary mb-5"></i>
-                        <h2 className="text-5xl mb-5 font-bold">72</h2>
+                        <h2 className="text-5xl mb-5 font-bold">
+                            <Counter className="count" end={72} />
+                        </h2>
+
                         <p className="text-xl">Countries</p>
                     </div>
 
                     <div className="item text-center leading-loose mb-10">
                         <i className="fa-solid fa-hands-holding-circle text-6xl text-primary mb-5"></i>
-                        <h2 className="text-5xl mb-5 font-bold">535+</h2>
+                        <h2 className="text-5xl mb-5 font-bold">
+                            <Counter className="count" end={535} suffix="+" />
+                        </h2>
                         <p className="text-xl">Complete Projects</p>
                     </div>
                     <div className="item text-center leading-loose">
                         <i className="fa-solid fa-users text-6xl text-primary mb-5"></i>
-                        <h2 className="text-5xl mb-5 font-bold">273+</h2>
+                        <h2 className="text-5xl mb-5 font-bold">
+                            <Counter className="count" end={273} suffix="+" />
+                        </h2>
                         <p className="text-xl">Happy Clients</p>
                     </div>
                     <div className="item text-center leading-loose">
                         <i className="fa-solid fa-medal text-6xl text-primary mb-5"></i>
-                        <h2 className="text-5xl mb-5 font-bold">432+</h2>
+                        <h2 className="text-5xl mb-5 font-bold">
+                            <Counter className="count" end={432} suffix="+" />
+                        </h2>
                         <p className="text-xl">Feedbacks</p>
                     </div>
                 </div>
